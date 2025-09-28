0df2c51ee2adfccf6b32484aaa8e92d35d007545
Yusuf <yusufhaque2020@gmail.com>
2025-09-28 16:18:10 +0530

    Add user.model.js

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,6 +1,6 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
-import User from "../models/User.js";
+import User from "../models/user.model.js";
 import "dotenv/config";
 
 // Create a client to send and receive events
