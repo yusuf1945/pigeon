3463046f53cd36d014708b2fcc128b7ffb46ba11
Yusuf <yusufhaque2020@gmail.com>
2025-09-28 14:02:00 +0530

    Authorization completed

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,5 +1,6 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db";
+import User from "../models/User.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "slack-clone" });
@@ -29,6 +30,7 @@ const deleteUserFromDB = inngest.createFunction(
   { id: "delete-user-from-db" },
   { event: "clerk/user.deleted" },
   async ({ event }) => {
+    await connectDB();
     const { id } = event.data;
     await User.deleteOne({ clerkId: id });
     //todo LATER
