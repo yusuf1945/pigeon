79b032316465ccfac086611780b6ecd634946e55
Yusuf <yusufhaque2020@gmail.com>
2025-10-07 12:30:15 +0530

    sentry corrected

--- backend/src/config/sentry.js
+++ backend/src/config/sentry.js
@@ -1,8 +1,9 @@
 import * as Sentry from "@sentry/node";
+import { SENTRY_DSN } from "./env.mjs";
 import "@sentry/tracing"; // only if you need performance tracing
 
 Sentry.init({
-  dsn: process.env.SENTRY_DSN,
+  dsn: SENTRY_DSN,
   integrations: [new Sentry.Integrations.Http({ tracing: true })],
   tracesSampleRate: 1.0, // adjust for production
 });
